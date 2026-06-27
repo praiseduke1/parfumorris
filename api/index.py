de31804c7b73ac995cea10754a82eb1b249fdc5c
Morris Parfum Dev <dev@morrisparfum.local>
2026-06-28 00:18:59 +0700

    chore: expose app variable in api/index.py for Vercel

--- api/index.py
+++ api/index.py
@@ -18,4 +18,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'parfumoray.settings')
 
 from django.core.wsgi import get_wsgi_application  # noqa: E402
 
-application = get_wsgi_application()
+app = get_wsgi_application()
