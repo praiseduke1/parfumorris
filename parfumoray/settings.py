647b39dd26545ea82e92b9bd17148cf4d6b94764
Morris Parfum Dev <dev@morrisparfum.local>
2026-06-27 23:18:17 +0700

    fix: ubah file logging handler ke streamhandler untuk vercel

--- parfumoray/settings.py
+++ parfumoray/settings.py
@@ -12,7 +12,7 @@ DEBUG = os.getenv('DEBUG', 'True').strip().lower() in ('true', '1', 'yes')
 
 if not SECRET_KEY and not DEBUG:
     raise RuntimeError('SECRET_KEY must be set in production!')
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'http://localhost:8000,http://127.0.0.1:8000').split(',')
 
